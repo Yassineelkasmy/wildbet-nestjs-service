9356eda41d082a8a9cd4f10a4d94a5013a8102f7
yassineelkasmy <yassineelkasmy@gmail.com>
2022-01-26 19:09:26 +0000

    Add modules scaffolding (League, Team, Round, Match)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LeagueModule } from './league/league.module';
+import { TeamModule } from './team/team.module';
+import { MatchModule } from './match/match.module';
+import { RoundModule } from './round/round.module';
 
 @Module({
-  imports: [],
+  imports: [LeagueModule, TeamModule, MatchModule, RoundModule],
   controllers: [AppController],
   providers: [AppService],
 })
