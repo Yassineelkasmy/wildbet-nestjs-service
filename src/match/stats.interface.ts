6d05a452dc1e337ddaa537cd53d8eba0e2241d5d
yassineelkasmy <yassineelkasmy@gmail.com>
2022-01-27 03:59:34 +0000

    Add stats interface to represent match stats (Results; including first half time results, second half time & penalties) correcting some mistypes ...

--- src/match/stats.interface.ts
+++ src/match/stats.interface.ts
@@ -1,10 +1,8 @@
-export interface Stats{
-    
-    home_score: number, //1
-    away_score: number, //3
-    ht_score: string, // "0-1"
-    ft_score: string, // "1-3"
-    et_score: string,
-    ps_score: null
-    
+export interface Stats{ 
+    home_score: number; //1
+    away_score: number; //3
+    ht_score: string; // "0-1"
+    ft_score: string; // "1-3"
+    et_score: string;
+    ps_score: string;
 }
